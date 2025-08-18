2c53cc48363cf162c10fa640023e691aaecf6d0f
BorgField <147969784+BorgField@users.noreply.github.com>
2025-08-18 18:01:15 +0800

    两个试剂半成品

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/potions/mini/PotionOfBurst.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/potions/mini/PotionOfBurst.java
@@ -4,11 +4,15 @@ import com.shatteredpixel.shatteredpixeldungeon.Challenges;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Barrier;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.FlavourBuff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.effects.FloatingText;
 import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfHealing;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.CharSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.ui.BuffIndicator;
+import com.watabou.noosa.Image;
 
 public class PotionOfBurst extends MiniPotion {
     //力量迸发试剂
@@ -19,13 +23,32 @@ public class PotionOfBurst extends MiniPotion {
     @Override
     public void apply(Hero hero) {
         identify();
+        Buff.affect(hero, burst.class);
+    }
 
-        if (Dungeon.isChallenged(Challenges.NO_HEALING)){
-            PotionOfHealing.pharmacophobiaProc(hero);
-        } else {
-            //~75% of a potion of healing
-            Buff.affect(hero, Barrier.class).setShield((int) (0.6f * hero.HT + 10));
-            hero.sprite.showStatusWithIcon( CharSprite.POSITIVE, Integer.toString((int) (0.6f * hero.HT + 10)), FloatingText.SHIELDING );
+    public static class burst extends Buff {
+        {
+            type = buffType.POSITIVE;
+        }
+
+        public static final float DURATION	= 20f;
+        @Override
+        public int icon() { return BuffIndicator.MINIPOTION; }
+
+        @Override
+        public void tintIcon(Image icon) { icon.hardlight(0.80f, 0, 0.13f); }
+
+        @Override
+        public float iconFadePercent() { return Math.max(0, (DURATION - visualcooldown()) / DURATION); }
+
+        @Override
+        public String toString() {
+            return Messages.get(this, "name");
+        }
+
+        @Override
+        public String desc() {
+            return Messages.get(this, "desc", dispTurns(visualcooldown()));
         }
     }
 }
